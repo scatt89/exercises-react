deb42ff725e89ba6811cbd85308bdd975618c6ee
mdasilva <dasilvagarciam@gmail.com>
2025-03-29 20:48:56 +0100

    draft country capital game

--- src/countryAndCapital/index.jsx
+++ src/countryAndCapital/index.jsx
@@ -1,3 +1,33 @@
+import { useState } from 'react';
+
+
+const getCountries = (data) => Object.keys(data);
+const getCapitals = (data) => Object.values(data);
+const shuffle = (list) => list.sort(() => Math.random() - 0.5);
+
+function Card(name, onClick) {
+    return <button>{name}</button>
+}
+
+function CountryCapitalGame({ data }) {
+    const items = shuffle(getCountries(data).concat(getCapitals(data)));
+    const [firstSelected, setFirstSelected] = useState([]);
+    const [secondSelected, setSecondSelected] = useState([]);
+
+    // I have no more time left but my idea was using the firstSelected and secondSelected to check if the user selected the correct answer
+    // and update the state color of the cards and the state of the game with another useState
+
+    return <div>
+        <ul>
+            {items.map((item, index) => (
+                <li key={index}>
+                    {Card(item)}
+                </li>
+            ))}
+        </ul>
+    </div>;
+}
+
 const CountryAndCapital = () => {
     return (
         <>
